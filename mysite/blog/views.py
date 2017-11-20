890e871dcf74e6a41c76ddbc8986a7131ce4cbf9
Sgt Steiner <futitotal@gmail.com>
2017-11-20 20:54:29 +0100

    Vistas basadas en clases

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
 
 # Create your views here.
@@ -20,7 +21,7 @@ def post_list(request):
     return render(request,
                   "blog/post/list.html",
                   {"page": page,
-                   "post": post})
+                   "posts": posts})
 
 
 def post_detail(request, year, month, day, post):
@@ -32,3 +33,10 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   "blog/post/detail.html",
                   {"post": post})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
